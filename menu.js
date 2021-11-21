99cb6872c5a26387f8c13202d22398e4228fec2a
nightcrawler2397 <94801836+nightcrawler2397@users.noreply.github.com>
2021-11-21 13:53:45 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,12 @@
+let iconMenu = document.getElementById("icon-menu");
+let menu = document.querySelector("#menu");
+
+iconMenu.addEventListener("click", ()=> {
+    menu.classList.toggle("mostrar");
+})
+alert("uno unounas");
+alert("no me sale la verdad");
+let uno = 50;
+let dos = 100;
+let resultado = dos / uno;
+alert("el resultado es " + resultado);
